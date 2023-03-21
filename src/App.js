ccfa0cb3550a0c3658822711a0064a78b288fe3c
Agailh <agailham86@gmail.com>
2023-03-21 11:30:01 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
 import Signup from "./Components/Signup";
+import Signin from "./Components/Signin";
+import Account from "./Components/Account";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Signup />
+      <div>
+        <h1 className="text-center text-3xl font-bold">Firebase</h1>
+        <Routes>
+          <Route path="/" element={<Signin />} />
+          <Route path="/" element={<Signup />} />
+          <Route path="/" element={<Account />} />
+        </Routes>
+      </div>
     </div>
   );
 }
--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -1,15 +1,7 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
 
 const Signup = () => {
-  return (
-    <div>
-      <h1 className="text-center text-3xl font-bold">Firebase</h1>
-      <Routes>
-        <Route path="/" element={<login />} />
-      </Routes>
-    </div>
-  );
+  return <div>Signup</div>;
 };
 
 export default Signup;
