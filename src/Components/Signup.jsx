884fb3f66a6b01f29e9b67ae2de8bc093cb3a936
Agailh <agailham86@gmail.com>
2023-03-21 12:05:01 +0700

    link

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
-  return <div className="max-w-[700px] mx-auto my-16 p-4">Sign in</div>;
+  return (
+    <div className="max-w-[700px] mx-auto my-16 p-4">
+      <div>
+        <h1 className="text-2xl font-bold py-2">Sign in to your account</h1>
+        <p className="py-2">
+          Already have an account?{" "}
+          <Link to="/" className="underline">
+            Sign In.
+          </Link>{" "}
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
