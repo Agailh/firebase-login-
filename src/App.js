564848cb6119d7ad7a85cb147132cff195a17743
Agailh <agailham86@gmail.com>
2023-03-22 11:59:40 +0700

    error still

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import { Route, Routes } from "react-router-dom";
 import Account from "./Components/Account";
 import Signin from "./Components/Signin";
 import Signup from "./Components/Signup";
+import { AuthContextProvider } from "./context/AuthContext";
 
 function App() {
   return (
     <div>
       <h1 className="text-center text-3xl font-bold">Firebase</h1>
-      <Routes>
-        <Route path="/" element={<Signin />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/account" element={<Account />} />
-      </Routes>
+      <AuthContextProvider>
+        <Routes>
+          <Route path="/" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/account" element={<Account />} />
+        </Routes>
+      </AuthContextProvider>
     </div>
   );
 }
